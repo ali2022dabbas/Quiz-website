3dada22308b388bff3f7afa6706f16d34bc43dab
Ali <ali2022dabbas@gmail.com>
2023-11-30 20:26:23 +0300

    CSS mostly complete

---
+++ script.js
@@ -0,0 +1,39 @@
+const quizData = [
+    {
+        question: 'How old is Florida?',
+        a: '10',
+        b: '17',
+        c: '26', 
+        d: '110',
+        correct: 'c'
+    }, {
+        question: 'What is the most used programming language in 2019?, 
+        a: 'Java', 
+        b: 'C', 
+        c: 'Python',
+        d: 'JavaScript', 
+        correct: 'd'
+    }, {
+        question: 'Who is the president of the USA?',
+        a: 'Florin Pop', 
+        b: 'Donald Trump', 
+        c: 'Ivan Saldano', 
+        d: 'Mihai Andrei',
+        correct: 'b'
+    }, {
+        question: 'What does HTML stand for?',
+        a: 'Hypertext Text Markup Language', 
+        b: 'Cascading Style Sheet', 
+        c: 'Jason Object Notation', 
+        d: 'Helicopters Terminals Motorboats Lamborghinis',
+        correct: 'a'
+    }, {
+        question: 'What year was JavaScript Launched?',
+        a: '1996', 
+        b: '1995', 
+        c: '1994', 
+        d: 'None of the above',
+        correct: 'd'
+    }
+];
+
